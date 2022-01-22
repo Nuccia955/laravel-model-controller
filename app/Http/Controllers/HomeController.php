10f883a28a796d306b2fc3c246a4a750efd7ff11
Nuccia955 <elenamunaro1995@gmail.com>
2022-01-22 14:17:35 +0100

    add Movie model + connect DB

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class HomeController extends Controller
-{
+{   
     public function index() {
+        $movies = Movie::all();
+        dump($movies);
         return view('home');
     }
 }
--- public/js/app.js
+++ public/js/app.js
@@ -19432,8 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\elena\Desktop\Boolean\Class#45\LARAVEL\laravel-movies\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\elena\Desktop\Boolean\Class#45\LARAVEL\laravel-movies\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\elena\Desktop\Boolean\Class#45\LARAVEL\laravel-model-controller\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\elena\Desktop\Boolean\Class#45\LARAVEL\laravel-model-controller\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
